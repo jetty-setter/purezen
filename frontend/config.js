3ca537966efd11401e349938f867c5b83b555d14
Steph Simmons <ssimmons86@gmail.com>
2026-05-01 15:37:50 -0500

    Update API_BASE_URL in config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.PUREZEN_CONFIG = {
-  API_BASE_URL: "http://pzalb-1467768001.us-east-1.elb.amazonaws.com",
+  API_BASE_URL: "http://178.105.54.156:8000",
   CHAT_ENDPOINT:               "/chat",
   SERVICES_ENDPOINT:           "/services",
   HEALTH_ENDPOINT:             "/health",
